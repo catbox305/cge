098722007462eeda5fc7dbcb7d6cf76702ac7e4a
catbox305 <lion712yt@gmail.com>
2023-02-17 21:44:25 -0800

    Updated files

--- cgePy/cgepy/ext/clear.py
+++ cgePy/cgepy/ext/clear.py
@@ -1,1 +1,4 @@
-print("\x1b[2J\x1b[H")
+__version__ = '0.6.5'
+
+def clear():
+	print("\x1b[2J\x1b[H")
---
+++ cgePy/cgepy/ext/text.py
@@ -0,0 +1,31 @@
+__version__ = '0.6.5'
+
+try:
+	from cgePy.cgepy.colors import *
+	from cgePy.cgepy.__init__ import *
+except ModuleNotFoundError:
+	try:
+		from cgepy.cgepy.colors import *
+		from cgepy.cgepy.__init__ import *
+	except ModuleNotFoundError:
+		from cgepy.colors import *
+		from cgepy.__init__ import *
+
+class _container:
+	pass
+
+class container:
+	def __init__(self):
+		self = _container
+	def empty(self):
+		self = _container()
+
+class Engine:
+	def __init__(self, grid: Grid):
+		self.grid = grid
+		self.bin = container()
+		self.temp = container()
+	def Insert(self, index, content):
+		self.temp.content = content
+		self.temp.contentlen = len(content)-1
+
