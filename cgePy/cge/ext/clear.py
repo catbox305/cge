bda83cdd8933833f0fea3b3204d1b0813c37b746
catbox305 <lion712yt@gmail.com>
2023-06-28 21:11:30 -0700

    Update clear.py

--- cgePy/cge/ext/clear.py
+++ cgePy/cge/ext/clear.py
@@ -1,6 +1,6 @@
 __version__ = '0.7.1'
 
 def clear():
-	'''Clears the screen.\n\nCongrats, you somehow managed to avoid using ext.clear!\n(Now you are using ext.clear.clear)'''
+	'''Clears the screen.\n\nCongrats, you somehow managed to avoid finding ext.clear and found ext.clear.clear instead!'''
 	print('\033[2J')
 	print('\033[0;0H')
